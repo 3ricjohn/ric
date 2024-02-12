d79176b9b25853346eec61d8a21c841b6011e972
3ricjohn <148600870+3ricjohn@users.noreply.github.com>
2024-02-12 13:50:19 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Eric from './Components/Eric';
+import Header from './Components/Header';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Eric />
+    <Header />
+  </React.StrictMode>,
+)
